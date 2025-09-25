a52805ac63c52b35eea69129cc309992bdab1029
MohitAryal <cozahitman@gmail.com>
2025-09-25 12:29:27 +0545

    Added function to chunk

--- app/pipeline.py
+++ app/pipeline.py
@@ -1,11 +1,18 @@
 from app.config import settings
 from pathlib import Path
 from app.helpers.preprocessing import preprocess_dataset
+from app.utils.chunker import chunk_job_descriptions
+
+raw_data_path = Path(settings.raw_data_dir) / settings.file_name
+processed_data_path = Path(settings.processed_data_dir) / settings.processed_file_name
+chunked_data_path = Path(settings.chunked_data_dir) / settings.chunked_file_name
+embeddings_file_path = Path(settings.embeddings_data_dir) / settings.embeddings_file_name
 
 # 1. Preprocess the dataset and store it as json
-processed_file = Path(settings.processed_data_dir) / settings.processed_file_name
-if not processed_file.exists():
-    data_path = Path(settings.raw_data_dir) / settings.file_name
-    preprocess_dataset(raw_data_path = data_path, processed_data_path=processed_file)
+if not processed_data_path.exists():
+    preprocess_dataset(raw_data_path = raw_data_path, processed_data_path=processed_data_path)
 
-# 2. Chunk the data to store to the vector db
+# 2. Chunk the data 
+if not chunked_data_path.exists():
+    print(f'\nCreating chunks...')
+    chunk_job_descriptions(processed_data_path=processed_data_path, chunk_path=chunked_data_path)
