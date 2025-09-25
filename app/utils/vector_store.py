52495f30d9b6ba6d27b71727a16305df2a3cf273
MohitAryal <cozahitman@gmail.com>
2025-09-26 00:01:27 +0545

    Populated vector db

--- app/utils/vector_store.py
+++ app/utils/vector_store.py
@@ -1,9 +1,58 @@
 from qdrant_client import QdrantClient
 from app.config import settings
-
-qdrant_client = QdrantClient(
-    url=settings.vector_db_url,
-    api_key=settings.vector_db_api_key,
+from typing import List, Dict, Any
+import pandas as pd
+import numpy as np
+from qdrant_client.models import (
+    Distance,
+    VectorParams,
+    PointStruct,
 )
 
-print(qdrant_client.get_collections())
+
+def get_qdrant_client():
+    qdrant_client = QdrantClient(
+        url=settings.vector_db_url,
+        api_key=settings.vector_db_api_key,
+        timeout=60.0
+    )
+    return qdrant_client
+
+
+def populate_vectordb(
+    client,
+    embeddings_path,
+    chunks_path,
+    batch_size: int = 100
+):
+    """
+    Populates a Qdrant collection with embeddings and metadata in batches.
+    """
+
+    embeddings = np.load(embeddings_path).tolist()
+    chunks = pd.read_json(chunks_path)
+    ids = chunks['chunk_id'].to_list()
+    metadatas = chunks['metadata'].to_list()
+
+    # Create collection
+    client.recreate_collection(
+        collection_name=settings.vector_db_collection_name,
+        vectors_config=VectorParams(size=settings.vector_size, distance=Distance.COSINE),
+    )
+
+    total = len(embeddings)
+    for start in range(0, total, batch_size):
+        end = min(start + batch_size, total)
+        batch_points = [
+            PointStruct(
+                id=int(ids[i]),
+                vector=embeddings[i],
+                payload=metadatas[i],
+            )
+            for i in range(start, end)
+        ]
+        client.upsert(
+            collection_name=settings.vector_db_collection_name,
+            points=batch_points,
+        )
+        print(f"Uploaded batch {start} to {end} ({end - start} vectors)")
