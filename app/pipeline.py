f91d66d404d5f3ee9ccd822f6fc9194f7ac67482
MohitAryal <cozahitman@gmail.com>
2025-09-26 00:02:08 +0545

    Added code to create collection in vector db

--- app/pipeline.py
+++ app/pipeline.py
@@ -1,9 +1,10 @@
 from app.config import settings
 from pathlib import Path
-from app.helpers.preprocessing import preprocess_dataset
-from app.utils.chunker import chunk_job_descriptions
-from app.helpers.embeddings import embed_chunks
-from app.utils.bm25 import create_bm25_retriever
+# from app.helpers.preprocessing import preprocess_dataset
+# from app.utils.chunker import chunk_job_descriptions
+# from app.helpers.embeddings import embed_chunks
+# from app.utils.bm25 import create_bm25_retriever
+from app.utils.vector_store import get_qdrant_client, populate_vectordb
 
 raw_data_path = Path(settings.raw_data_dir) / settings.file_name
 processed_data_path = Path(settings.processed_data_dir) / settings.processed_file_name
@@ -27,6 +28,13 @@ if not embeddings_data_path.exists():
     print('Genrating embeddings')
     embed_chunks(chunk_path=chunked_data_path, embed_path=embeddings_data_path)
 
+# 4. Populate keyword retriever
 if not keyword_retriever_path.exists():
     print('Initailizing keyword retriever')
     create_bm25_retriever(chunks_path=chunked_data_path, storage_path=keyword_retriever_path)
+
+# 5. Populate vector database
+client = get_qdrant_client()
+if not client.collection_exists(collection_name=settings.vector_db_collection_name):
+    print('\nPopulating vector database...')
+    populate_vectordb(client, embeddings_path=embeddings_data_path, chunks_path=chunked_data_path)
