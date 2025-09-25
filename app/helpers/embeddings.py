412a233715a1f2c4156659d8919b12ec23bf800c
MohitAryal <cozahitman@gmail.com>
2025-09-25 15:00:56 +0545

    Added embedding logic

--- app/helpers/embeddings.py
+++ app/helpers/embeddings.py
@@ -1,8 +1,30 @@
-from sentence_transformers import SentenceTransformer
 from app.config import settings
+import numpy as np
+import pandas as pd
+import math
+from app.utils.embedding_function import embed_function
 
-embedder = SentenceTransformer(settings.embedding_model)
+embedding_dim = settings.embedding_dimension
+batch_size = 128
 
-def embed_chunks(chunks: int) :
-    embedded = embedder.encode(chunks)
-    return embedded
+
+def embed_chunks(chunk_path, embed_path) :
+    df = pd.read_json(chunk_path)
+    chunks = df['content'].to_list()
+
+    total_chunks = len(chunks)
+    total_batches = math.ceil(total_chunks / batch_size)
+    
+    # Create memmap file
+    embeddings_memmap = np.lib.format.open_memmap(
+        embed_path, mode='w+', dtype=np.float16, shape=(total_chunks, embedding_dim)
+    )
+
+    # Loop to write batches of embedded chunks in the memmap
+    for i in range(0, total_chunks, batch_size):
+        batch_texts = chunks[i:i+batch_size]
+        batch_embeddings = embed_function(batch_texts)
+        embeddings_memmap[i:i+len(batch_texts)] = batch_embeddings
+    
+    print('\nEmbeddings complete')
+    
