d013859dca0e9130824f0c311b06f379b034358d
MohitAryal <cozahitman@gmail.com>
2025-09-28 17:30:25 +0545

    Added message as response logic

--- app/main.py
+++ app/main.py
@@ -1,17 +1,27 @@
 from fastapi import FastAPI
-from models import QueryRequest, QueryResponse
+from app.models import QueryRequest, QueryResponse, MessageResponse
 from app.inference_pipeline import run_pipeline
+from app.config import logger
+from typing import Union
 
-app = FastAPI(
+router = FastAPI(
     title="Job Search RAG API",
     description="Hybrid search + reranking + LLM-enriched results for job search",
 )
 
 
-@app.post("/api/query", response_model=QueryResponse)
+@router.post("/api/query", response_model=Union[QueryResponse, MessageResponse])
 def query_jobs(request: QueryRequest):
     
-    results = run_pipeline(request)
-    response = QueryResponse(results=results)
+    logger.info('Sending request to pipeline')
+    results = run_pipeline(request.query)
 
-    return response
+    logger.info('Results found Returning them')
+    
+    if isinstance(results, str):
+        # It's a message like "out of scope" or "no match"
+        logger.info('Sending message as response')
+        return {"message": MessageResponse(llm_response)}
+    
+    logger.info('Sending result as response')
+    return {'results': QueryResponse(results)}
