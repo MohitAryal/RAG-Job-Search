021aaa61aeef1fd0d33d83bff068bcdcbf8081a7
MohitAryal <cozahitman@gmail.com>
2025-09-28 18:15:11 +0545

    Added with structured ouptut

--- app/services/LLM_integration.py
+++ app/services/LLM_integration.py
@@ -1,23 +1,24 @@
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import JsonOutputParser
+from app.models import QueryResponse
 from app.config import settings
 
-def llm_result(results, query):
-    llm = ChatGroq(
-        model=settings.llm_model,
-        temperature=settings.llm_temperature,
-        max_tokens=settings.llm_max_tokens,
-        groq_api_key=settings.llm_api_key
-        )
+llm = ChatGroq(
+    model=settings.llm_model,
+    temperature=settings.llm_temperature,
+    max_tokens=settings.llm_max_tokens,
+    groq_api_key=settings.llm_api_key
+    )
 
+
+def llm_result(results, query):
     prompt = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
-                '''You are a highly skilled job search assistant. Your task is to extract and summarize the most relevant job listings for the user based on their query and provided job data.
+                '''You are a highly skilled job search assistant. Your task is to get the most relevant job listings for the user based on their query and provided job data.
                 Use the job data provided. Do not hypothesize anything.
-                If the query is not related to a job data search, be sincere and just return that it's not your expertise(no need to adhere to the output format for this case).
+                If the query is not related to a job data search, say that it's not your expertise.
                 If no job is returned or no returned job matches the user's query, apolozize and say that such type of job doesn't exist in the database.
 
     User query:
@@ -29,37 +30,18 @@ def llm_result(results, query):
     ### Instructions:
 
     1. **Job Selection**: From the list of job data, identify the top 3 jobs that are most relevant to the user's query.
-    - Carefully respect filters in the query such as location, remote preference, experience level, job title, or required skills if mentioned in the query.
-    - If the query includes terms like "remote only", "3+ years of experience", or "roles in USA", make sure the selected jobs match these conditions. If this means there's no match, just apolozize and select the results without the filter.
+    - If the query includes terms like "remote only", "3+ years of experience", or "roles in USA", try to match the selected jobs match these conditions.
 
-    2. **Content Snippet Extraction**:
-    - For each selected job, extract the 2–3 most relevant sentences (maximum of 400 characters in total per job) that strongly align with the query.
-    - Prioritize mention of key elements from the query: skills, seniority level, location, remote work, or tech stack.
-    - Keep the text concise and directly quoted from the job description. Do not paraphrase.
-
-    3. **Explanation**:
-    - For each selected job, provide a 1-sentence explanation for why it was chosen.
-
-    4. **Output Format**:
-    - Return a JSON list containing 3 items, each representing one selected job.
-    - Each item should include the following fields:
-        - `ID`: The id of relevant chosen job present in the provided Job data. Return exact job id provided.
-        - `content_snippet`: Extracted text from the job description (quoted, ≤500 characters).
-        - `explanation`: One sentence describing why the job is relevant to the query.
+    2. **Explanation**:
+    - For each selected job, provide a 1-2 sentence(max 75 characters) explanation for why it was chosen.
     ]
     '''
             )
         ]
-    )
+    ).with_structured_output(QueryResponse)
 
-    chain = prompt | llm | JsonOutputParser()
-    
-    # Keys you want to keep
-    keys_to_keep = {'ID', 'Job Title', 'Job Location', 'Job Level', 'Job Description'}
+    chain = prompt | llm  
 
-    # Extract only those keys from each dictionary
-    filtered_result = [{k: d[k] for k in keys_to_keep if k in d} for d in results]   
-
-    response = chain.invoke({'query': query, 'results': filtered_result})
+    response = chain.invoke({'query': query, 'results': results})
 
     return response
