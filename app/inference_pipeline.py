5f2fa9b3a20273021bc9dea6de29da72e8fb140c
MohitAryal <cozahitman@gmail.com>
2025-09-28 17:32:37 +0545

    Added logging

--- app/inference_pipeline.py
+++ app/inference_pipeline.py
@@ -1,16 +1,20 @@
 from app.services.hybrid_search import perform_hybrid_search
 from app.services.reranker import rerank_jobs
 from app.services.LLM_integration import llm_result
+from app.config import logger
 
 
 def run_pipeline(query: str):
     # 1. Search for relevant documents
+    logger.info('searching')
     search_result = perform_hybrid_search(query=query)
 
     # 2. Rerank the search results and retrieve top k results
+    logger.info('reranking')
     reranked_result = rerank_jobs(job_ids=search_result, query=query)
 
     # 3. Call LLM to enrich the results
+    logger.info('enriching')
     llm_output = llm_result(reranked_result, query)
 
     return llm_output
